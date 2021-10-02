7282a25c836ea78d629ff43db431331100f44eb3
Eugen <varlaea2@icloud.com>
2021-10-03 00:54:48 +0300

    add AdminsAPP

---
+++ admins/views.py
@@ -0,0 +1,61 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
+from geekshop.mixin import CustomDispatchMixin
+from users.models import User
+
+
+def index(request):
+    return render(request, 'admins/admin.html')
+
+
+class UserListView(ListView, CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+    context_object_name = 'users'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['title'] = 'Админка | Пользователи'
+        return context
+
+
+class UserCreateView(CreateView, CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy('admins:admins_user')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'Админка | Регистрация'
+        return context
+
+
+class UserUpdateView(UpdateView, CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy('admins:admins_user')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'Админка | Редактирование'
+        return context
+
+
+class UserDeleteView(DeleteView, CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admins_user')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 import document as document
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -27,6 +27,9 @@ urlpatterns = [
     path('products/', include('mainapp.urls', namespace='mainapp')),
     path('users/', include('users.urls', namespace='users')),
     path('baskets/', include('baskets.urls', namespace='baskets')),
+    path('admins/', include('admins.urls', namespace='admins')),
+
+
 ]
 
 if settings.DEBUG:
