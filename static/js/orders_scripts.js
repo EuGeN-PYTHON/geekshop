53f2f839d5ffdb6ba60e19ac9e50033c3f02c08a
Eugen <varlaea2@icloud.com>
2021-10-27 15:51:52 +0300

    update 'price' on order_form.html (for change product)

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -1,4 +1,131 @@
-window.addEventListener("load", () => {
+// window.addEventListener("load", () => {
+//
+//
+//         let _quantity, _price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost;
+//
+//         let quantity_arr = []
+//         let price_arr = []
+//
+//         let total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val())
+//         console.log(total_forms)
+//
+//
+//         let order_total_quantity = parseInt($('.order_total_quantity').text()) || 0;
+//         let order_total_price = parseInt($('.order_total_cost').text().replace(',', '.')) || 0;
+//
+//         for (let i = 0; i < total_forms; i++) {
+//             _quantity = parseInt($('input[name=orderitems-' + i + '-quantity').val());
+//             _price = parseInt($('.orderitems-' + i + '-price').text().replace(',', '.'));
+//
+//             console.log(_price)
+//             quantity_arr[i] = _quantity
+//             if (_price) {
+//                 price_arr[i] = _price;
+//
+//             } else {
+//                 price_arr[i] = 0
+//             }
+//         }
+//
+//
+//         $('.order_form').on('click', 'input[type=number]', (e) => {
+//             let target = e.target;
+//             orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
+//             if (price_arr[orderitem_num]) {
+//                 orderitem_quantity = parseInt(target.value)
+//                 delta_quantity = orderitem_quantity - quantity_arr[orderitem_num];
+//                 quantity_arr[orderitem_num] = orderitem_quantity;
+//                 orderSummerUpdate(price_arr[orderitem_num], delta_quantity)
+//             }
+//         });
+//
+//         $('.order_form').on('click', 'input[type=checkbox]', (e) => {
+//             let target = e.target;
+//             orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
+//             if (target.checked) {
+//                 delta_quantity = -quantity_arr[orderitem_num];
+//             } else {
+//                 delta_quantity = quantity_arr[orderitem_num];
+//             }
+//             orderSummerUpdate(price_arr[orderitem_num], delta_quantity)
+//         });
+//
+//         $('.order_form select').change((e) => {
+//             //     let target = e.target;
+//             //     orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+//             //     console.log(orderitem_num)
+//             //     console.log(target.value)
+//             //
+//             //     $.ajax({
+//             //         url: '/orders/reform/'+target.value+'/',
+//             //         success: function (data){
+//             //             let price_html = '<span class="orderitems-' + orderitem_num + '-price">' + data.price.toString().replace('.', ',') + ' руб </span>';
+//             //
+//             //             let current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
+//             //             current_tr.find('td:eq(2)').html(price_html);
+//             //         },
+//             //     });
+//             //     e.preventDefault()
+//             // });
+//
+//             let target = event.target;
+//             orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+//             // console.log(orderitem_num)
+//             // console.log(target.value)
+//             let orderitem_product_pk = target.options[target.selectedIndex].value;
+//             console.log(orderitem_product_pk)
+//
+//             if (orderitem_product_pk) {
+//                 $.ajax({
+//                     url: '/orders/reform/' + orderitem_product_pk + '/',
+//                     success: function (data) {
+//                         if (data.price) {
+//                             price_arr[orderitem_num] = parseFloat(data.price)
+//                             if (isNaN(quantity_arr[orderitem_num])) {
+//                                 quantity_arr[orderitem_num] = 0;
+//                             }
+//                             let price_html = '<span class="orderitems-' + orderitem_num + '-price">' + data.price.toString().replace('.', ',') + ' руб</span>';
+//                             let current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
+//                             current_tr.find('td:eq(2)').html(price_html);
+//                         }
+//                     }
+//
+//                 });
+//             }
+//         });
+//
+//
+//         $('.formset_row').formset({
+//             addText: 'ДОБАВИТЬ ПРОДУКТ',
+//             deleteText: 'УДАЛИТЬ',
+//             prefix: 'ORDERITEMS',
+//             removed: deleteOrderItem,
+//         });
+//
+//         function deleteOrderItem(row) {
+//             let target_name = row[0].querySelector('input[type="number"]').name;
+//             orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+//             delta_quantity = -quantity_arr[orderitem_num];
+//             orderSummerUpdate(price_arr[orderitem_num], delta_quantity)
+//         }
+//
+//
+//         function orderSummerUpdate(orderitem_price, delta_quantity) {
+//             delta_cost = orderitem_price * delta_quantity
+//             order_total_price = Number((order_total_price + delta_cost).toFixed(2));
+//             order_total_quantity = order_total_quantity + delta_quantity;
+//             $('.order_total_quantity').html(order_total_quantity.toString())
+//             $('.order_total_cost').html(order_total_price.toString() + ',00');
+//
+//         }
+//
+//
+//     }
+// )
+// ;
+
+
+window.onload = function () {
 
     let _quantity, _price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost;
 
@@ -11,6 +138,7 @@ window.addEventListener("load", () => {
 
     let order_total_quantity = parseInt($('.order_total_quantity').text()) || 0;
     let order_total_price = parseInt($('.order_total_cost').text().replace(',', '.')) || 0;
+
 
     for (let i = 0; i < total_forms; i++) {
         _quantity = parseInt($('input[name=orderitems-' + i + '-quantity').val());
@@ -26,10 +154,12 @@ window.addEventListener("load", () => {
         }
     }
 
+    // console.info('PRICE',price_arr)
+    // console.info('QUANTITY',quantity_arr)
 
 
-    $('.order_form').on('click', 'input[type=number]', (e) => {
-        let target = e.target;
+    $('.order_form').on('click', 'input[type=number]', function () {
+        let target = event.target;
         orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
         if (price_arr[orderitem_num]) {
             orderitem_quantity = parseInt(target.value)
@@ -39,40 +169,22 @@ window.addEventListener("load", () => {
         }
     });
 
-    $('.order_form').on('click', 'input[type=checkbox]', (e) => {
-        let target = e.target;
+    $('.order_form').on('click', 'input[type=checkbox]', function () {
+        let target = event.target;
         orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
         if (target.checked) {
             delta_quantity = -quantity_arr[orderitem_num];
-        }else{
+        } else {
             delta_quantity = quantity_arr[orderitem_num];
         }
-         orderSummerUpdate(price_arr[orderitem_num], delta_quantity)
+        orderSummerUpdate(price_arr[orderitem_num], delta_quantity)
     });
-
-    $('.order_form select').change( (e) => {
-        let target = e.target;
-        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
-        console.log(orderitem_num)
-        console.log(target.value)
-
-        $.ajax({
-            url: '/orders/reform/'+target.value+'/',
-            success: function (data){
-                $('.orderitems-' + orderitem_num + '-price').html(data.result)
-            },
-        });
-        e.preventDefault()
-    });
-
-
-
 
 
     $('.formset_row').formset({
-        addText: 'ДОБАВИТЬ ПРОДУКТ',
-        deleteText: 'УДАЛИТЬ',
-        prefix: 'ORDERITEMS',
+        addText: 'добавить продукт',
+        deleteText: 'удалить',
+        prefix: 'orderitems',
         removed: deleteOrderItem,
     });
 
@@ -92,4 +204,33 @@ window.addEventListener("load", () => {
         $('.order_total_cost').html(order_total_price.toString() + ',00');
 
     }
-});
+
+    // $('.order_form select').change(function () {
+    $(document).on('change', '.order_form select', function () {
+
+        let target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+        // console.log(orderitem_num)
+        let orderitem_product_pk = target.options[target.selectedIndex].value;
+        console.log(orderitem_product_pk)
+
+        if (orderitem_product_pk) {
+            $.ajax({
+                url: '/orders/reform/' + orderitem_product_pk + '/',
+                success: function (data) {
+                    if (data.price) {
+                        price_arr[orderitem_num] = parseFloat(data.price)
+                        if (isNaN(quantity_arr[orderitem_num])) {
+                            quantity_arr[orderitem_num] = 0;
+                        }
+                        let price_html = '<span class="orderitems-' + orderitem_num + '-price">' + data.price.toString().replace('.', ',') + '</span> руб';
+                        let current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
+                        current_tr.find('td:eq(2)').html(price_html);
+                    }
+                }
+
+            });
+        }
+    });
+
+}
